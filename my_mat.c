98cd8ea28853277256cc77624135df453bf96d9f
shahar143 <shaharuli52@gmail.com>
2022-11-26 11:50:08 +0200

    the beginning

---
+++ my_mat.c
@@ -0,0 +1,7 @@
+//
+// Created by shahar on 11/22/22.
+//
+
+#include "my_mat.h"
+
+
---
+++ my_mat.h
@@ -0,0 +1,8 @@
+//
+// Created by shahar on 11/22/22.
+//
+
+#ifndef MATALA2_MY_MAT_H
+#define MATALA2_MY_MAT_H
+
+#endif //MATALA2_MY_MAT_H
